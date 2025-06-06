827b0146a1d1d701b07a8a9a0719fd3e92881ded
Madeeha923 <madeehaarif15@gmail.com>
2025-06-06 09:14:55 +0530

    Update settings.py

--- sdm/settings.py
+++ sdm/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-b)9s_9n!y)7j9wti*2ih!eww&dc6@vds!tt&e%vnc1@w0rerl7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studentdatamanagement.onrender.com']
+ALLOWED_HOSTS = ['studentdatamanagement.onrender.com', 'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
